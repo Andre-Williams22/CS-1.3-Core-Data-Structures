80085df1e1418a4aa11783bc05a9fd1807dfea9a
Andre Williams <awjrs22@gmail.com>
2020-02-04 12:06:10 -0800

    finishes contains function

--- Code/strings.py
+++ Code/strings.py
@@ -5,14 +5,18 @@ def contains(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
-    p1 = 0
-    p2 = 0
-    for i in range(len(text)):
-        if p1[i] == p2[i]:
-            return True
+    if len(pattern) <= len(text):
+        pass
+    else:
+        return False
+
+    for i in range(len(pattern)):
+        if text[i] == pattern[i]:
+            i += 1
         else:
-            p1[i] += 1
-    return False 
+            return False 
+    return True
+
 
 
 def find_index(text, pattern):
@@ -21,12 +25,20 @@ def find_index(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
-    for i in range(0, text):
-        if i == pattern[i]:
-            return text[i]
+    if len(pattern) <= len(text):
+        pass
+    else:
+        return False
+
+    for i in range(len(pattern)):
+        if text[i] == pattern[i]:
+            #i += 1
+            return text.index(text[i])
         else:
             i+=1
-    return None
+
+
+
 
 def find_all_indexes(text, pattern):
     """Return a list of starting indexes of all occurrences of pattern in text,
@@ -34,6 +46,7 @@ def find_all_indexes(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+
 
 
 def test_string_algorithms(text, pattern):
@@ -47,23 +60,26 @@ def test_string_algorithms(text, pattern):
     print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
 
 
-def main():
-    """Read command-line arguments and test string searching algorithms."""
-    import sys
-    args = sys.argv[1:]  # Ignore script file name
-    if len(args) == 2:
-        text = args[0]
-        pattern = args[1]
-        test_string_algorithms(text, pattern)
-    else:
-        script = sys.argv[0]
-        print('Usage: {} text pattern'.format(script))
-        print('Searches for occurrences of pattern in text')
-        print("\nExample: {} 'abra cadabra' 'abra'".format(script))
-        print("contains('abra cadabra', 'abra') => True")
-        print("find_index('abra cadabra', 'abra') => 0")
-        print("find_all_indexes('abra cadabra', 'abra') => [0, 8]")
+# def main():
+#     """Read command-line arguments and test string searching algorithms."""
+#     import sys
+#     args = sys.argv[1:]  # Ignore script file name
+#     if len(args) == 2:
+#         text = args[0]
+#         pattern = args[1]
+#         test_string_algorithms(text, pattern)
+#     else:
+#         script = sys.argv[0]
+#         print('Usage: {} text pattern'.format(script))
+#         print('Searches for occurrences of pattern in text')
+#         print("\nExample: {} 'abra cadabra' 'abra'".format(script))
+#         print("contains('abra cadabra', 'abra') => True")
+#         print("find_index('abra cadabra', 'abra') => 0")
+#         print("find_all_indexes('abra cadabra', 'abra') => [0, 8]")
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+#print(contains('abc', 'ab'))
+print(find_index('abc', 'bc'))
