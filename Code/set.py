faa81838e77a489a43b6c3dcf78d17b0ae7d5975
Andre Williams <awjrs22@gmail.com>
2020-02-26 16:47:59 -0800

    adds sets test and sets file

---
+++ Code/set.py
@@ -0,0 +1,33 @@
+
+
+def __init__(elements=None):
+    ''' initialize a new empty set structure, and add each element if a sequence is given '''
+    pass 
+
+
+def size():
+    '''property that tracks the number of elements in constant time '''
+
+
+def contains(element):
+    ''' return a boolean indicating whether element is in this set '''
+
+def add(element):
+    ''' add element to this set, if not present already '''
+
+
+def remove(element):
+    '''remove element from this set, if present, or else raise KeyError '''
+
+def union(other_set):
+    '''return a new set that is the union of this set and other_set'''
+
+
+def intersection(other_set):
+    '''return a new set that is the intersection of this set and other_set  '''
+
+def difference(other_set):
+    '''return a new set that is the difference of this set and other_set '''
+
+def is_subset(other_set):
+    '''return a boolean indicating whether other_set is a subset of this set'''
